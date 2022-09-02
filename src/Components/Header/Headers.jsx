557dd02c25f12af0f256274cb913bb355adace83
Hafiz Hamza Ali <HamzaAli.900900@gmail.com>
2022-09-02 19:05:39 +0500

    Working on header

--- src/Components/Header/Headers.jsx
+++ src/Components/Header/Headers.jsx
@@ -4,7 +4,8 @@ import { makeStyles } from '@mui/styles';
 import { Typography, Box } from '@mui/material';
 import Image from '../../Images/brandBanner.jpg'
 import Navbar from './Navbar';
-
+import { Theme } from '../Theme';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 const useStyles = makeStyles((theme) => ({
   headerWrapper: {
     width: "100%",
@@ -13,21 +14,51 @@ const useStyles = makeStyles((theme) => ({
     background: `linear-gradient(to bottom right,#04303140,#00606473),url(${Image})`,
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
+  },
+  headerContainer: {
+    width: "100%",
+    minHeight: "90vh",
+    height: "auto",
+    display: "flex",
+    color: Theme.colors.basic2,
+    alignItems: "flex-start",
+    justifyContent: "center",
+    flexFlow: "column wrap",
+    // padding: "20px"
+  },
+  headerBody:{
     padding:"20px"
   },
-  headerContainer:{
-    width: "100%",
-    minHeight:"90vh",
-    height: "auto",
-    display:"flex",
-    alignItems:"flex-start",
-    justifyContent:"center",
-    flexFlow:"column wrap"
+  decorator: {
+    display: "flex",
+    fontWeight:"bolder",
+    // fontFamily:"roboto",
+    alignItems:"center",
+    position:"relative",
+    marginTop:"20px",
+    "&:before":{
+      backgroundColor:Theme.colors.primary1,
+      height:"50px",
+      width:"50px",
+      content:'""',
+      borderRadius:"50%"
+    }
   },
-  headerTitle:{
-    textAlign:'left'
-
+  decoratorText: {
+    // marginLeft:"-40px",
+    position: "absolute",
+    left: "30px",
+    fontSize:"20px"
+  },
+  decoratorArrow: {
+    position: "absolute",
+    left: "130px",
+    fontSize:"small",
+    animationName:"upDown",
+    animationDuration:"0.5s",
+    animationIterationCount:"infinite"
   }
+
 }))
 
 const Headers = () => {
@@ -38,13 +69,22 @@ const Headers = () => {
     <Box className={classes.headerWrapper}>
       <Navbar />
       <Box className={classes.headerContainer}>
-        <Typography variant='h3'component="h4" className={classes.headerTitle}>
-          Hello I'm MERN stack developer
-        </Typography>
-        <Typography variant='h5'component="h4" className={classes.headerDesc}>
-          I create websites and applications based on your needs
-        </Typography>
+        <Box className={classes.headerBody}>
+          <Typography variant='h3' component="h4" className={classes.headerTitle}>
+            Hello I'm MERN stack developer
+          </Typography>
+          <Typography variant='h5' component="h4" className={classes.headerDesc}>
+            I create websites and applications based on your needs
+          </Typography>
+          <Box className={classes.decorator}>
+            <Typography variant='span' className={classes.decoratorText}>About me</Typography>
+            <Typography variant='span' className={classes.decoratorArrow}>
+              <ArrowDownwardIcon />
+            </Typography>
+          </Box>
+        </Box>
       </Box>
+
     </Box>
   )
 }
